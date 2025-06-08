2ec17d4526f5aee9152f03eb644568ca80f931e8
finalSay9 <chimwazaevan1@gmail.com>
2025-06-08 15:35:33 +0200

    ok

--- src/DB/models.py
+++ src/DB/models.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from sqlalchemy import Column, DateTime, Integer, String, Boolean, Column, Date, DateTime, Enum, Float, ForeignKey, Integer, String, Text
+from sqlalchemy import Column, DateTime, Integer, String, Boolean, Column, Date, DateTime, Enum, Float, ForeignKey, Integer, String, Text, func
 from src.DB.database import Base
 from sqlalchemy.orm import relationship
-from src.DB.schema import UserRole
+from src.DB.schema import UserRole 
 
 #model for user 
 
@@ -22,6 +22,36 @@ class Create_User(Base):
     password = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, nullable=True, onupdate=datetime.utcnow)
+    reset_token =Column(String, nullable=True)
+    reset_token_expire=Column(String, nullable=True)
+
+
+class Department(Base):
+    __tablename__ = 'departments'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    employees = relationship('Employee', back_populates="departments")
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), unique=True)
+    department_id = Column(Integer, ForeignKey("departments.id"))
+    position = Column(String)
+    hire_date = Column(DateTime)
+    salary = Column(Float)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    user = relationship("User", back_populates="employee")
+    department = relationship("Department", back_populates="employees")
 
 
 
